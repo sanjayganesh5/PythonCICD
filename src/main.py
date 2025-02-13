acfb8d563a90f31da8338d931f2eb8ee78557900
sanjayganesh5 <106419569+sanjayganesh5@users.noreply.github.com>
2025-02-13 22:48:22 +0530

    add new function

--- src/main.py
+++ src/main.py
@@ -2,3 +2,8 @@ def main_method(*args, **kwargs):
     print(f"Arguments: {args}")
     print(f"Keyword Arguments: {kwargs}")
     return kwargs
+
+
+def new_func():
+    print("Do something")
+    return "Something is done!"
